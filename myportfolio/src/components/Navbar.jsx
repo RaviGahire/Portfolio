e2a37e1871466a915662971a088173338406c7c5
RaviGahire <ravigahire3@gmail.com>
2024-05-18 15:09:21 +0530

    navbar component & Home componet design finish

--- myportfolio/src/components/Navbar.jsx
+++ myportfolio/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useRef } from "react"
+
 import "../Css/navbar.css"
 import gsap from 'gsap'
 import { useGSAP } from "@gsap/react"
-import { NavLink } from "react-router-dom"
+import { NavLink ,Link } from "react-router-dom"
 
 
 
@@ -10,6 +11,7 @@ gsap.registerPlugin(useGSAP)
 
 const Navbar = () => {
     const tl = useRef()
+    const rotate = useRef()
 
     const openMenu = () => {
         tl.current.play();
@@ -31,26 +33,41 @@ const Navbar = () => {
                 opacity: 0,
                 stagger: 0.2
             })
+    
         tl.current.pause()
 
-    })
+    });
+
+
 
     return (
         <>
             <header>
                 <div className="nav">
                     <h1 className="logo"> Ravi Gahire <i class="ri-sun-fill"></i></h1>
+
                     <i class="ri-menu-line" onClick={openMenu}></i>
                 </div>
                 <div className="side-menu">
-                <li><NavLink to="/"   className="nav-links">Home</NavLink></li>
-                    <li><NavLink to="./work"   className="nav-links">My Work</NavLink></li>
-                    <li><NavLink to="./about"   className="nav-links">About </NavLink></li>
-                    <li><NavLink to="./blog"   className="nav-links"> Blog</NavLink></li>
-                    <li><NavLink to="./contact"   className="nav-links">Contact Me</NavLink></li>
-
+                    <li><NavLink to="/" className="nav-links">Home</NavLink></li>
+                    <li><NavLink to="./work" className="nav-links">My Work</NavLink></li>
+                    <li><NavLink to="./about" className="nav-links">About </NavLink></li>
+                    <li><NavLink to="./blog" className="nav-links"> Blog</NavLink></li>
+                    <li><NavLink to="./contact" className="nav-links">Contact Me</NavLink></li>
                     <i class="ri-close-line" onClick={closeMenu}></i>
+
+                    <div className="down-buttons">
+                        <Link to="/about">
+                        <button class="button-89" role="button">Hire Me</button>
+                        </Link>
+                        <Link to="/about">
+                        <button class="button-89" role="button">Contact</button>
+                        </Link>
+                    </div>
                 </div>
+
+
+
             </header>
         </>
     )

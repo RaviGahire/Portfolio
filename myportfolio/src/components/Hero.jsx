821645fc38e8bcb435a6d86259cbd512cbb5d54b
RaviGahire <ravigahire3@gmail.com>
2024-11-11 15:40:57 +0530

    change fonts and working on hero section

--- myportfolio/src/components/Hero.jsx
+++ myportfolio/src/components/Hero.jsx
@@ -3,17 +3,25 @@ import { Link } from 'react-router-dom'
 import '../custom-css-files/hero.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-
-
 const Hero = () => {
     return (
         <>
-            <div className="container hero">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, omnis quas illum pariatur aliquam praesentium quaerat quia veniam doloremque ducimus, dolores iure expedita sint quos earum nemo consequuntur laudantium delectus!</p>
-            </div>
+           <section className="hero-section d-flex justify-content-center align-items-center">
+                <div className="container text-center">
+                    <div className="hero-content">
+                        <h1 className="display-1">Welcome to my portfolio</h1>
+                        <p className="lead"><span className='text-info'>I am</span> <span className='typing'>Web dev</span></p>
+                        <Link href="#" className="btn btn-primary btn-lg mt-3 ">Resume</Link>
+                        <div className="social-icons mt-5">
+                            <Link to="https://www.facebook.com/ravi.gahire" target='/' className='icon' aria-label="Facebook"><i class="ri-facebook-circle-line"></i></Link>
+                            <Link to="https://x.com/RaviGahire" target='/' className='icon' aria-label="Twitter"><i class="ri-twitter-x-line"></i></Link>
+                            <Link to="" target='/' className='icon' aria-label="Instagram"><i class="ri-instagram-line"></i></Link>
+                            <Link to="https://www.linkedin.com/in/ravi-gahire-7855b110a/" target='/' className='icon' aria-label="LinkedIn"><i class="ri-linkedin-box-line"></i></Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
-
 export default Hero;

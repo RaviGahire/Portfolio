12be5d2254ed3616d1d8048967ddd3cbcd9da87c
RaviGahire <ravigahire3@gmail.com>
2025-11-03 23:41:42 +0530

    trying to create new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useState } from "react"
+
 
 export const App = () => {
+const [scroll , setScroll]= useState('')
+
+
   return (
-    <div className='text-5xl'>App</div>
+    <>
+      <aside className="bg-gray-900 hidden md:max-w-[330px] lg:block  min-h-screen fixed inset-0">
+
+
+
+      </aside>
+      <main className="bg-amber-200 max-w-7xl mx-auto  p-5">hello</main>
+
+    </>
   )
 }

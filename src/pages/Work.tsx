865bf8c56b514581e58c830f4b83f89066f49a9c
RaviGahire <ravigahire3@gmail.com>
2026-07-10 16:51:07 +0530

    refactor: Move ProjectDetail and ProjectsGrid components to a new directory structure

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,27 +1,28 @@
 import { IconLayoutGrid } from "@tabler/icons-react";
 import { Badge } from "../components/common/Badge";
 import { Heading } from "../components/common/Heading";
-import { ProjectsGrid } from "../components/common/work/ProjectsGrid";
-import { div } from "motion/react-client";
-import { ProjectDetail } from "../components/common/work/ProjectDetail";
+import { ProjectDetail } from "../components/work/ProjectDetail";
+import { ProjectsGrid } from "../components/work/ProjectsGrid";
 
 export const Work = () => {
   return (
     <section aria-label="work-or-project-section" className="h-auto bg-bg-dark">
-      <div >
+      <div>
         <Badge
           title="Portfolio"
           icon={<IconLayoutGrid stroke={2} size={18} />}
         />
 
-        <Heading
-          title="Selected Work"
-          subTitle="A collection of projects I've built and designed with care craft, and attention to detail."
-        />
-      
+        <div className="flex justify-center items-center ">
+          {" "}
+          <Heading
+            title="Selected Work"
+            subTitle="A collection of projects I've built and designed with care craft, and attention to detail."
+          />
+        </div>
       </div>
-        {/* <ProjectsGrid /> */}
-        <ProjectDetail/>
+      <ProjectsGrid />
+      {/* <ProjectDetail /> */}
     </section>
   );
 };

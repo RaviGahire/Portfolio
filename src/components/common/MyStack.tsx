b3174e10c44975073c81c1485982892915df74b0
RaviGahire <ravigahire3@gmail.com>
2026-07-11 21:54:44 +0530

    feat: Add MyStack component to display skill images

---
+++ src/components/common/MyStack.tsx
@@ -0,0 +1,30 @@
+import { skillImages } from "../../data/AllData";
+
+export const MyStack = () => {
+  return (
+    <div className="flex justify-center items-center w-full py-4 bg-bg-dark text-white mt-10">
+      <div className="w-1/5 flex justify-end">
+        <h4 className="text-2xl font-semibold uppercase font-poppins mr-4">
+          My Stack
+        </h4>
+      </div>
+      <div className="border border-dark-borders w-full" />
+      {/* skills iamges */}
+    <div className="w-full flex justify-around items-center gap-4">
+  {skillImages.map((skill) => (
+    <div
+      key={skill.name} 
+      className="size-10 shrink-0 rounded-full overflow-hidden"
+      title={skill.name}
+    >
+      <img 
+        className="size-full object-cover" 
+        src={skill.img} 
+        alt={`${skill.name} icon`} 
+      />
+    </div>
+  ))}
+</div>
+    </div>
+  );
+};

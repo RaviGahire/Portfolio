cd310175a9ea51a5a097a3952017b88ddc72652b
RaviGahire <ravigahire3@gmail.com>
2026-07-12 18:20:06 +0530

    refactor: Update Badge component to improve styling and make icon prop optional

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -2,18 +2,22 @@ import type { ReactNode } from "react";
 
 type Badgeprops = {
   title: string;
-  icon: ReactNode;
+  icon?: ReactNode;
 };
 
 export const Badge = ({ title, icon }: Badgeprops) => {
   return (
-    <div className="inline-flex items-center gap-3 rounded-full border border-white/10 bg-white/5 px-5 py-1.5 shadow-lg backdrop-blur-xl transition-all duration-300 hover:border-lavender/40 hover:shadow-lavender/20">
-      <div className="rounded-full bg-linear-to-br from-lavender/20 to-purple-500/20 p-1 text-lavender">
-        {icon}
-      </div>
-      <span className="font-poppins text-sm font-extralight tracking-[0.08em] text-white">
-        {title || "Provide title!"}
-      </span>
-    </div>
+      <div className="inline-flex rounded-full bg-[#7f22fe]/10 border-[#7f22fe]/30 border border-solid px-4 py-1.5 items-center gap-2">
+              {icon ? (<>
+              <div>{icon}</div>
+              </>) : (
+                <span className="size-1.5 rounded-full bg-(image:--secondary-gradient) animate-pulse" />
+              )
+
+              }
+              <span className="font-semibold  font-poppins text-secondary-text text-sm ">
+                {title}
+                </span>
+            </div>
   );
 };

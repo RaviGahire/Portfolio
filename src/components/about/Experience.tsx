444aa50339050b071238de14b3ed6ea51d0c67b4
RaviGahire <ravigahire3@gmail.com>
2026-07-20 21:42:40 +0530

    refactor: Simplify Experience component structure and enhance styling

--- src/components/about/Experience.tsx
+++ src/components/about/Experience.tsx
@@ -1,6 +1,6 @@
-import { motion, number } from "motion/react";
+import { motion } from "motion/react";
 
-export const Experience = ({ expData }) => {
+export const Experience = ({ expData}) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-12 gap-4 md:gap-16 items-center px-4">
       {/* Image */}
@@ -17,7 +17,7 @@ export const Experience = ({ expData }) => {
             alt="mypic"
             className="w-full h-full mask-b-from-10% transform hover:scale-110 object-cover grayscale group-hover:grayscale-0 transition-all duration-700 rounded-xl"
           />
-          <div className="absolute left-0 right-0 bottom-0 p-8">
+          <div className="absolute left-0 right-0 bottom-0 p-8 transform -translate-x-40 group-hover:translate-x-0 transition-transform duration-700 ease-linear ">
             <p className="uppercase text-base tracking-[5px] text-lavender mb-1 font-semibold font-elmsSans">
               based in
             </p>
@@ -28,7 +28,7 @@ export const Experience = ({ expData }) => {
           </div>
         </div>
 
-        {/*  */}
+      
       </motion.div>
       {/* EXPERIENCE */}
       <div className="md:col-span-7 flex flex-col justify-center mt-8 md:mt-0">
@@ -42,7 +42,7 @@ export const Experience = ({ expData }) => {
               transition={{ delay: index * 0.2 }}
               className="group flex gap-4 md:gap-3 items-start border-b border-white/5 pb-3 md:pb-4"
             >
-              <span className="text-green-500 font-mono text-sm pt-1 md:pt-2">
+              <span className="text-purple font-elmsSans text-xl ">
                 0{expData.length - index} 
               </span>
               <div>
@@ -59,7 +59,9 @@ export const Experience = ({ expData }) => {
             </motion.div>
           ))}
         </div>
-      </div>
+
+         </div>
+         
     </div>
   );
 };

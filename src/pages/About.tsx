f4ac5dbca137d9c52a420a81dca094550eef0d56
RaviGahire <ravigahire3@gmail.com>
2026-07-20 21:38:13 +0530

    refactor: Improve structure and styling in About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,33 @@
 import { Experience } from "../components/about/Experience";
 import { ExperienceData } from "../data/AllData";
 import { Heading } from "../components/common/Heading";
+import { Badge } from "../components/common/Badge";
+import { MyStack } from "../components/common/MyStack";
 
 export const About = () => {
   return (
-    <section aria-label="about-me-section" className="bg-bg-dark h-auto py-10">
-      <div className="max-w-7xl mx-auto ">
+    <section
+      id="about"
+      aria-label="about-me-section"
+      className="bg-bg-dark h-auto py-10"
+    >
+      <div>
         {/* Heading */}
-        <div className="py-10 ">
-          <Heading title="About me" />
+        <div className="py-10 flex flex-col justify-center items-center ">
+          <div className="mb-4">
+            <Badge title="bit about me " />
+          </div>
+
+          <Heading title="About me" subTitle="hello " />
         </div>
         {/* Experience */}
         <div>
           <Experience expData={ExperienceData} />
         </div>
+        {/* my stack */}
+             <div className="px-4">
+                 <MyStack/>
+             </div>
       </div>
     </section>
   );

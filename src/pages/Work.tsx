1bbe1c919c10af7cd7a8e5b8cb80f62978cd061a
RaviGahire <ravigahire3@gmail.com>
2026-07-20 21:42:24 +0530

    refactor: Clean up Work component structure and improve readability

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -2,45 +2,49 @@ import { IconLayoutGrid } from "@tabler/icons-react";
 import { Badge } from "../components/common/Badge";
 import { Heading } from "../components/common/Heading";
 import { ProjectsGrid } from "../components/work/ProjectsGrid";
-import { projectData } from "../data/AllData"
+import { projectData } from "../data/AllData";
 import { useState } from "react";
 import { ProjectDetail } from "../components/work/ProjectDetail";
 
 export const Work = () => {
-const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState(null);
 
   return (
-    <section aria-label="work-or-project-section" className="relative min-h-auto bg-bg-dark py-8">
+    <section
+      id="work"
+      aria-label="work-or-project-section"
+      className="relative min-h-auto bg-bg-dark py-8"
+    >
       <div className="flex flex-col justify-center items-center">
-        <Badge
-          title="Portfolio"
-          icon={<IconLayoutGrid stroke={2} size={18} />}
-        />
-
-        <div>
-          {" "}
+        {/* Badge */}
+        <div className="mb-4">
+          <Badge
+            title="My Work"
+            icon={<IconLayoutGrid stroke={2} size={18} />}
+          />
+        </div>
+        {/* Heading */}
+        <div className="mb-4">
           <Heading
             title="Selected Work"
             subTitle="A collection of projects I've built and designed with care craft, and attention to detail."
           />
         </div>
+        
       </div>
       {/* Project card grid */}
-      <ProjectsGrid 
-      projects={projectData}
-      onProjectClick={setSelectedProject}
+      <ProjectsGrid
+        projects={projectData}
+        onProjectClick={setSelectedProject}
       />
 
       {/* Project details */}
       {selectedProject && (
         <ProjectDetail
-        project={selectedProject}
-        onClose={() => setSelectedProject(null)}
+          project={selectedProject}
+          onClose={() => setSelectedProject(null)}
         />
-
-      )
-      }
-
+      )}
     </section>
   );
 };

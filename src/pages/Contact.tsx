ee3494a72770e2a6164db803f62130979b1110ac
RaviGahire <ravigahire3@gmail.com>
2026-07-23 16:52:41 +0530

    feat: Implement Contact page layout with Badge, Heading, and ContactForm components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,20 @@
-
+import { Badge } from "../components/common/Badge";
+import { Heading } from "../components/common/Heading";
+import { ContactForm } from "../components/contact/ContactForm";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="contact" aria-label="contact-section" className="bg-zinc-950 border-t border-dark-borders pt-8">
+      {/* heading */}
+      <div className="flex justify-center items-center">
+        <Badge title="Drop a message and I'll get back to you." />
+      </div>
+      {/* Heading */}
+      <div className="flex justify-center items-center mt-4">
+        <Heading title="Contact me" />
+      </div>
+      {/* contact */}
+      <ContactForm />
+    </section>
+  );
+};

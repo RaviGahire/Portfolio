f47e75c1dfc58f998fe292c4c952bb3ddcedb227
RaviGahire <ravigahire3@gmail.com>
2026-07-24 22:02:03 +0530

    refactor: Enhance type safety in Work component by specifying Project type for selectedProject state

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -5,22 +5,25 @@ import { ProjectsGrid } from "../components/work/ProjectsGrid";
 import { projectData } from "../data/AllData";
 import { useState } from "react";
 import { ProjectDetail } from "../components/work/ProjectDetail";
+import { type Project } from "../data/AllData";
 
 export const Work = () => {
-  const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
 
   return (
     <section
       id="work"
       aria-label="work-or-project-section"
-      className="relative min-h-auto bg-zinc-950 py-8 border-t border-dark-borders"
+      className="relative bg-zinc-950 py-8 border-t border-dark-borders min-h-auto md:min-h-screen"
     >
       <div className="flex flex-col justify-center items-center">
         {/* Badge */}
         <div className="my-4">
           <Badge
             title="My work"
-            icon={<IconLayoutGrid className="text-white" stroke={2} size={18} />}
+            icon={
+              <IconLayoutGrid className="text-white" stroke={2} size={18} />
+            }
           />
         </div>
         {/* Heading */}
@@ -29,9 +32,9 @@ export const Work = () => {
             title="Selected Work"
             subTitle="A collection of projects I've built and designed with care craft, and attention to detail."
           />
-        </div>     
+        </div>
       </div>
-      
+
       {/* Project card grid */}
       <ProjectsGrid
         projects={projectData}

49d586ffe7cd5ad591bc27eed535ebe8b2d4fe45
RaviGahire <ravigahire3@gmail.com>
2026-07-24 22:01:13 +0530

    refactor: Enhance styling and structure in ContactForm component for improved usability and consistency

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,21 +1,25 @@
 export const ContactForm = () => {
   return (
     <div className="flex justify-center items-center  bg-neutral-950 font-inter font-medium text-gray-300 mt-6 p-4">
-      <form action="submit" className=" w-full max-w-200 flex flex-col gap-6 h-100">
+      <form
+        action="submit"
+        className=" w-full max-w-200 flex flex-col gap-6 h-100"
+      >
         {/* Top Inputs */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <input
             type="text"
             name="userName"
             placeholder="Your good name"
-            className="w-full bg-neutral-950 border border-dark-borders/50  rounded-lg px-4 py-3 text-white placeholder-secondary-text focus:outline-none focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-300"
+            required
+            className="text-[14px] font-light w-full bg-neutral-950 border border-dark-borders/50  rounded-lg px-4 py-3 text-white placeholder-secondary-text focus:outline-none focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-300"
           />
           <input
             type="email"
             name="email"
             required
             placeholder="Email or X-ID"
-            className="w-full bg-neutral-950 border border-dark-borders/50  rounded-lg px-4 py-3 text-white placeholder-secondary-text focus:outline-none focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-200"
+            className="text-[14px] font-light w-full bg-neutral-950 border border-dark-borders/50  rounded-lg px-4 py-3 text-white placeholder-secondary-text focus:outline-none focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-200"
           />
         </div>
 
@@ -24,17 +28,20 @@ export const ContactForm = () => {
           <textarea
             name="message"
             placeholder="Your Message"
-            className="w-full bg-neutral-950 border border-dark-borders/50 
+            className="text-[14px] font-light w-full bg-neutral-950 border border-dark-borders/50 
             rounded-lg px-4 py-3 text-white placeholder-secondary-text focus:outline-none
-            focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-200 resize-none"></textarea>
+            focus:border-lavender/50 focus:ring-0.5 focus:ring-lavender transition-all duration-200 resize-none"
+          ></textarea>
         </div>
 
         {/* Submit Button */}
         <div className="flex justify-center items-center">
           <button
             type="submit"
-            className="w-fit cursor-pointer bg-white text-black font-semibold rounded-lg 
-            px-4 py-3 hover:bg-gray-200 active:scale-[0.98] transition-all duration-200">
+            className="text-secondary-text hover:text-primary-text w-fit cursor-pointer font-semibold rounded-lg bg-white hover:bg-(image:--vite-gradient) 
+            bg-size-[200%_200%] bg-left  hover:bg-right  transition-[background-position]  duration-500 ease-linear
+            px-4 py-3 hover:bg-gray-200 active:scale-[0.98]"
+          >
             Send Message
           </button>
         </div>
